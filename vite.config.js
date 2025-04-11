231348305ec4721364c6ff2bd6cdd5a9da6ddbe9
Priyanshu1502 <priyanshubhatt1502@gmail.com>
2025-04-11 10:06:58 +0530

    added allowed host all

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: [
-      "c454607d-832c-4437-9e40-e8a9b8f7bb70-00-2ctshlt1fo9h6.pike.replit.dev",
-    ],
+    allowedHosts: "all",
   },
 });

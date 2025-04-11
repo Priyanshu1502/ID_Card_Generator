b6116f09021a23b15a3fc3f6892f240fb1e624eb
Priyanshu1502 <priyanshubhatt1502@gmail.com>
2025-04-11 09:52:08 +0530

    added repit access

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      "c454607d-832c-4437-9e40-e8a9b8f7bb70-00-2ctshlt1fo9h6.pike.replit.dev",
+    ],
+  },
 });
